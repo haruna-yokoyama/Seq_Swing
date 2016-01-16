a12dc82febe1654af07e5970ce88f2ef8b1feca1
haruna-yokoyama <cs12097@s.inf.shizuoka.ac.jp>
2016-01-16 12:06:23 +0900

    commit

---
+++ src/SequenceDiagram/ResultTrace.java
@@ -0,0 +1,63 @@
+package SequenceDiagram;
+
+import java.util.List;
+
+import com.sun.jdi.Field;
+import com.sun.jdi.Value;
+
+public class ResultTrace {
+	private List<String> methodName;
+	private List<String> declaringType;
+	private List<String> returnType;
+	private List<String> argumentType;
+	private Field fieldName;
+	private Value valueName;
+
+
+	public List<String> getMethodName() {
+		return methodName;
+	}
+	public List<String> setMethodName(List<String> methodName) {
+		return this.methodName = methodName;
+	}
+
+
+	public List<String> getDeclaringType() {
+		return declaringType;
+	}
+	public List<String> setDeclaringType(List<String> declaringType) {
+		return this.declaringType = declaringType;
+	}
+
+
+	public List<String> getReturnType() {
+		return returnType;
+	}
+	public List<String> setReturnType(List<String> returnType) {
+		return this.returnType = returnType;
+	}
+
+
+	public List<String> getArgumentType() {
+		return argumentType;
+	}
+	public List<String> setArgumentType(List<String> argumentType) {
+		return this.argumentType = argumentType;
+	}
+
+
+	public Field getFieldName() {
+		return fieldName;
+	}
+	public Field setFieldName(Field fieldName) {
+		return this.fieldName = fieldName;
+	}
+
+
+	public Value getValueName() {
+		return valueName;
+	}
+	public Value setValueName(Value valueName) {
+		return this.valueName = valueName;
+	}
+}
